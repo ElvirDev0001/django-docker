bee8d04a2ee704a11d4220021ccbb1dac0491fdd
ElvirDev0001 <dev.elvir.github@gmail.com>
2024-02-04 19:21:06 +0100

    allowed hosts

--- mywebpage/settings.py
+++ mywebpage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p4fio6y2l6y%ad-nditdxq)uq3wtb-zxgv04)dp)&q=_otzdn@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elvir.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
